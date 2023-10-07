199594ef2419a72b1466cf94a058188ff3ceedd6
Kate Horbenko <adfrw445577@gmail.com>
2023-10-07 20:24:38 +0300

    added second task of the first lection

---
+++ Lection1/Task2/Task2.py
@@ -0,0 +1,6 @@
+number1=int(input("Type first number"))
+number2=int(input("Type second number"))
+print("First numb:",number1 )
+print("Second num:",number2)
+result= number1 + number2
+print("Sum two numbers:", result)
