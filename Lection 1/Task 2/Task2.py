97cbe349b73b1e0f634a2f61062c97a7cb492d19
Kate Horbenko <adfrw445577@gmail.com>
2023-10-07 20:14:08 +0300

    Added first task of the first lection.

--- Lection 1/Task 2/Task2.py
+++ Lection 1/Task 2/Task2.py
@@ -1,2 +1,6 @@
-Usernumb=int(input("Type two number"))
-print("Sum two numbers:")
+number1=int(input("Type first number"))
+number2=int(input("Type second number"))
+print("First numb:",number1 )
+print("Second num:",number2)
+result= number1 + number2
+print("Sum two numbers:", result)
