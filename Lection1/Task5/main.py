dc0e3fb463fcc62fc5de20d0ebda19735fbce8cc
Kate Horbenko <adfrw445577@gmail.com>
2023-11-24 00:14:40 +0200

    Added first task of the first lection.

--- Lection1/Task5/main.py
+++ Lection1/Task5/main.py
@@ -4,21 +4,6 @@ def quadratic_equation(a, b, c):
     dis = b**2 - 4*a*c
     if a == 0:
         return "If a = 0, the equation becomes linear"
-    if b == 0:
-        root = -c / a
-        if root > 0:
-            root1 = math.sqrt(root)
-            root2 = -math.sqrt(root)
-            return root1, root2       
-        elif root < 0:
-            return "There is no decision"     
-    if c == 0 and b == 0:
-        root = 0/a
-        return root
-    elif c == 0:
-        root1 = 0
-        root2 = (-b)/a
-        return root1, root2
     if dis > 0:
         root1 = (-b + dis**0.5) / (2*a)
         root2 = (-b - dis**0.5) / (2*a)
@@ -26,8 +11,6 @@ def quadratic_equation(a, b, c):
     elif dis == 0:
         root = -b / (2*a)
         return root
-
-        
 
 a = float(input("enter coefficient a: "))
 b = float(input("enter coefficient b: "))
