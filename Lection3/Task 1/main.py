bf4ec873d4b0c40c987b6e005b2a02f72e044ec8
Kate Horbenko <adfrw445577@gmail.com>
2023-11-29 16:06:05 +0200

    Adds a back character counter to the first one

--- Lection3/Task 1/main.py
+++ Lection3/Task 1/main.py
@@ -1,4 +1,6 @@
 symbol = {'letters':0 , 'number':0 , 'Special characters':0}
+letter_counter = {}
+
 your_file = input("Enter a file name:")
 
 with open(your_file, "r") as file:
@@ -10,5 +12,11 @@ with open(your_file, "r") as file:
             symbol['number'] += 1
         else:
             symbol['Special characters'] += 1
+    for symbole in text:
+        if symbole in letter_counter:
+            letter_counter[symbole] += 1
+        else:
+            letter_counter[symbole] = 1
 
 print(f"The text contains: {symbol}")
+print(f"print: {letter_counter}")
