aa1c05dc77632f5de231698ddbc5280178dca255
Kate Horbenko <adfrw445577@gmail.com>
2023-11-29 15:58:43 +0200

    Reduces the number of variables in the third task

--- Lection1/Task 3/main.py
+++ Lection1/Task 3/main.py
@@ -5,14 +5,15 @@ def fahrenheit (fahr):
 
 print ("1 Сelsius to fahrenheit \n2 to fahrenheit to celsius:")
 choice_translation = input("Select an action: ")
+num = float(input("Enter number:"))
 
 match choice_translation:
     case "1":
-        cel = float(input("Enter celsius: "))
-        fahr = celsius (cel)
+        #cel = float(input("Enter celsius: "))
+        fahr = celsius (num)
         print ("Fahrenheit:", fahr)
     case "2":
-        fahr = float(input("Enter fahrenheit: "))
-        cel = fahrenheit (fahr)
+        #fahr = float(input("Enter fahrenheit: "))
+        cel = fahrenheit (num)
         print ("Celsius:", cel)
         
